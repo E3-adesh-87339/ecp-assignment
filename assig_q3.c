74caa2e17dcf881f2bd060b7e855cdf4ca5f455b
sunbeam <aadeshbhawar7777@gmail.com>
2024-08-31 17:35:22 +0530

    assignment 1 question 3 completed

---
+++ assig_q3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/*Print the ASCII value of user entered character in decimal, hex, octal format and also print the
+character for user entered ASCII value.
+*/
+
+
+int main()
+{
+ char ch;
+ printf("enter the character \n");
+ scanf("%c",&ch);
+ printf("ascii value of character is %d\n hexa value is %x\n octol value is %o\n decimal value is %d\n",ch,ch,ch,ch);
+ return 0;
+
+
+
+
+}
