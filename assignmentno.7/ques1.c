512152f00675bd9eeff23f292b441c7b89baec6d
Adesh-Bhawar <aadeshbhawar7777@gmail.com>
2024-09-18 14:04:00 +0530

    assignment 9 and 10 done

--- assignmentno.7/ques1.c
+++ assignmentno.7/ques1.c
@@ -2,18 +2,41 @@
 array. Re-use these functions in rest of the assignments wherever required.*/
 
 #include<stdio.h>
+
+int a(int *arr[][3]);
+int p(int *arr[][3]);
 int main()
 {
+
+	
 	int arr[3][3];
+	a(arr);
+	p(arr);
+
+return 0;
+}
+
+int a(int *arr[][3])
+{
 	printf("enter the arry element\n");
 	for (int i = 0; i<3 ;i++)
 	{
 		for (int j = 0;j<3;j++)
 		{
-		scanf("%d%d"&i,&j);
+		scanf("%d",&arr[i][j]);
 		}
 	printf("\n");
+}
+}
+int p(int *arr[][3])
+{
+printf("array element are:\n");
+for (int i=0;i<3;i++)
+{
+	for (int j=0;j<3;j++)
+	{
+		printf("%d ",arr[i][j]);
 	}
-
-return 0;
+printf("\n");
+}
 }
---
+++ assignmentno.7/ques2.c
@@ -0,0 +1,19 @@
+//Write functions to calculate addition, subtraction, multiply two matrices of 3x3.
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[3][3];
+	int j;
+	printf("enter the 3X3 array\n");
+	for (int i= 0; i<3; i++){
+	for (int j=0;j<3;j++)
+	{
+	scanf("%d",arr[i],[j]);
+	}
+	printf("\n");
+	}
+
+return 0;
+}
